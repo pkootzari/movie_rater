b5d7117672b08267eaaf8f71a45339fcfe2a4750
pkootzari <pkootzari@gmail.com>
2020-09-05 10:24:55 +0330

    creating models

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Movie(models.Model) :
+    title = models.CharField(max_length=30),
+    description = models.TextField()
+
+    class Meta:
+        unique_together = ['title']
+        indexes = [
+            models.Index(fields=['title', 'description'])
+        ]
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='ratings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings')
+
+    class Meta:
+        unique_together = ['movie', 'user']
+        indexes = [
+            models.Index(fields=['movie', 'user'])
+        ]
